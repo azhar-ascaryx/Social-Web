d6967316746bf7f856ab78983cd4cf15b58e4d89
Mohit Raj <agrmohit@outlook.com>
2022-05-25 16:39:20 +0530

    backend: Fix typos in routes/posts.js

--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -19,9 +19,9 @@ router.put("/:id", async (req, res) => {
     const post = await Post.findById(req.params.id);
     if (post.userId === req.body.userId) {
       await post.updateOne({ $set: req.body });
-      res.status(200).json("the post has been updated");
+      res.status(200).json("The post has been updated");
     } else {
-      res.status(403).json("you can update only your post");
+      res.status(403).json("You can only update your post");
     }
   } catch (err) {
     res.status(500).json(err);
@@ -34,9 +34,9 @@ router.delete("/:id", async (req, res) => {
     const post = await Post.findById(req.params.id);
     if (post.userId === req.body.userId) {
       await post.deleteOne();
-      res.status(200).json("the post has been deleted");
+      res.status(200).json("The post has been deleted successfully");
     } else {
-      res.status(403).json("you can delete only your post");
+      res.status(403).json("You can only delete your post");
     }
   } catch (err) {
     res.status(500).json(err);
